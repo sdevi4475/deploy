053b1f42e91d1dc11b2fce9453ed6d00dd742379
sdevi4475 <sdevi4475@gmail.com>
2025-10-10 12:25:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,1 @@
-st.title("welcome streamlit")
-st.header("this is header")
-st.subheader("this is subheader")
-formula1 = ''' a+b '''
-st.latex(formula1)
-formula = ''' (a+b)2 =  a2+b2+2ab '''
-st.latex(formula)
-python_code = '''
-     a = 9
-     b=4
-     c=78
-     v = a+b+c
-     print(v)
-
-    '''
-st.code(python_code, language='python')
-st.header("Python")
-st.caption("python is good lang ")
+!pip install streamlit
