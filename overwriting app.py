5a754f1346f7dc0b64b2254193d9ac64b2526b95
sdevi4475 <sdevi4475@gmail.com>
2025-10-10 12:44:18 +0530

    Update overwriting app.py

--- overwriting app.py
+++ overwriting app.py
@@ -1,11 +1,11 @@
-%%writefile app.py
+# button widgets
+%%writefile app1.py
 import streamlit as st
-st.image('/content/LINEar bias.jpg' ,caption = " flower",width=500)
-st.header("display flower video")
-loc =open("/content/flower_vid.mp4",'rb')
-vid_bytes = loc.read()
-st.video(vid_bytes)
-# st.header("display flower auideo")
-# loc =open("/content/flower_vid.mp4",'rb')
-# vid_bytes = loc.read()
-# st.audio(vid_bytes,format=audio/ogg)
+st.subheader("radio")
+st.caption("click the button below")
+agree = st.checkbox("agree",value=True)
+ml = st.checkbox("machine")
+if agree:
+  st.write("yes")
+if ml:
+    st.write("yes machine learning")
